91b63cc040728b556ce12a737c330bfe717d637c
Arkadiusz Dobosz <a.dobosz@codeandpepper.com>
2020-01-04 22:49:50 +0100

    added board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box, CssBaseline } from "@material-ui/core";
+import Board from "./components/Board/Board";
 
 const App: React.FC = () => {
   return (
     <Box>
       <CssBaseline />
+      <Board />
     </Box>
   );
 };
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+
+function Board() {
+  return (
+    <Box>
+      <Typography>Game Board</Typography>
+    </Box>
+  );
+}
+
+export default Board;
